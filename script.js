19545fc10ad444f5bd948b3f3e8f7fb255fb563c
enoch wu <enoch.wu@kingsway.school.nz>
2022-01-17 16:26:19 +1300

    fixed bug: username didn't allow

--- script.js
+++ script.js
@@ -7,15 +7,14 @@ const firebaseConfig = {
     messagingSenderId: "867698747892",
     appId: "1:867698747892:web:922cb4fc1c0d0cfdd5f1a1"
 };
-$(document).ready(function () {
-    if (localStorage.getItem("chatUsername") === null) {
-        var username = prompt("Enter a username");
-        localStorage.setItem("chatUsername", username)
-    } else {
-        var username = localStorage.getItem("chatUsername");
-        alert("Your username is " + username);
-    }
-})
+
+if (localStorage.getItem("chatUsername") === null) {
+    var username = prompt("Enter a username");
+    localStorage.setItem("chatUsername", username)
+} else {
+    var username = localStorage.getItem("chatUsername");
+    alert("Your username is " + username);
+}
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 document.getElementById("send-message").addEventListener("submit", postChat);
