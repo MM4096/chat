76ae08784785911e243a6ab288cfb0252d0c1b72
enoch wu <enoch.wu@kingsway.school.nz>
2022-01-19 20:41:40 +1300

    added alias and updated to v1.3

--- script.js
+++ script.js
@@ -17,10 +17,10 @@ if (localStorage.getItem("chatUsername") === null || localStorage.getItem("chatU
 }
 
 //add comments infront of above statement to enable/disable access
-if (username != "wu" && username != "bot") {
-    window.alert("System Currently Shutdown. Please try again later");
-    window.location.href = "https://www.google.com/";
-}
+//if (username != "wu" && username != "bot") {
+//    window.alert("System Currently Shutdown. Please try again later");
+//    window.location.href = "https://www.google.com/";
+//}
 
 
 firebase.initializeApp(firebaseConfig);
@@ -29,25 +29,48 @@ document.getElementById("send-message").addEventListener("submit", postChat);
 function postChat(e) {
     e.preventDefault();
     const timestamp = Date.now();
-    const chatTxt = document.getElementById("chat-txt");
-    if (chatTxt != "" && chatTxt!= " ") {
-        const message = chatTxt.value;
-        chatTxt.value = "";
-        db.ref("messages/" + timestamp).set({
-            usr: username,
-            msg: message,
-        });
-        $("#chat-btn").prop('disabled', true);
+    let chatTxt = document.getElementById("chat-txt");
+    const message = chatTxt.value;
+    if (message != "" && message != " " && message != null) {
+        if (message.includes("alias")) {
+            let command = message.split(" ");
+            console.log(command);
+            if (command.length != 3) {
+                window.alert("Error. See console for more info");
+                console.error("Error. Expected 2 arguments.")
+            } else {
+                localStorage.setItem(command[1], command[2]);
+                console.log(localStorage.getItem(command[1]));
+            }
+            $("#chat-txt").val("")
+        } else {
+            
+            chatTxt.value = "";
+            db.ref("messages/" + timestamp).set({
+                usr: username,
+                msg: message,
+            });
+            $("#chat-btn").prop('disabled', true);
+            setTimeout(function () { $("#chat-btn").prop("disabled", false); }, 3000);
+        }
+        
     } else {
         window.alert("Error. See console for more info");
         console.error("Error. Expected string, got '' instead")
     }
     
-    setTimeout(function () { $("#chat-btn").prop("disabled", false); }, 3000);
+    
 }
 const fetchChat = db.ref("messages/");
 fetchChat.on("child_added", function (snapshot) {
     const messages = snapshot.val();
-    const msg = "<li>" + messages.usr + " : " + messages.msg + "</li>";
-    document.getElementById("messages").innerHTML += msg;
+    if (localStorage.getItem(messages.usr) == null) {
+        const msg = "<li>" + messages.usr + " : " + messages.msg + "</li>";
+        document.getElementById("messages").innerHTML += msg;
+    } else {
+        let aliasName = localStorage.getItem(messages.usr);
+        const msg = "<li>" + aliasName + " : " + messages.msg + "</li>";
+        document.getElementById("messages").innerHTML += msg;
+    }
+    
 });
